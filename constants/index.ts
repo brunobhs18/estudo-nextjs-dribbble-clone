b2210c578ec114e740d15d359750a14dab9f688a
Bruno Henrique <brunobhs18@gmail.com>
2023-07-04 15:05:03 -0300

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,97 @@
+export const NavLinks = [
+    { href: '/', key: 'Inspiration', text: 'Inspiration' },
+    { href: '/', key: 'Find Projects', text: 'Find Projects' },
+    { href: '/', key: 'Learn Development', text: 'Learn Development' },
+    { href: '/', key: 'Career Advancement', text: 'Career Advancement' },
+    { href: '/', key: 'Hire Developers', text: 'Hire Developers' }
+  ];
+  
+  export const categoryFilters = [
+    "Frontend",
+    "Backend",
+    "Full-Stack",
+    "Mobile",
+    "UI/UX",
+    "Game Dev",
+    "DevOps",
+    "Data Science",
+    "Machine Learning",
+    "Cybersecurity",
+    "Blockchain",
+    "E-commerce",
+    "Chatbots"
+  ]
+  
+  export const footerLinks = [
+    {
+      title: 'Para desenvolvedores',
+      links: [
+        'Go Pro!',
+        'Explorar o trabalho de desenvolvimento',
+        'blog de desenvolvimento',
+        'Code podcast',
+        'Projetos open-source',
+        'Indique um Amigo',
+        'Código de Conduta',
+      ],
+    },
+    {
+      title: 'Contrate desenvolvedores',
+      links: [
+        'Anunciar uma vaga de emprego',
+        'Publicar um projeto freelance',
+        'Pesquisar por desenvolvedores',
+      ],
+    },
+    {
+      title: 'Marcas',
+      links: [
+        'Anuncie conosco',
+      ],
+    },
+    {
+      title: 'Empresa',
+      links: [
+        'Sobre',
+        'Carreiras',
+        'Apoiar',
+        'Kit de mídia',
+        'Depoimentos',
+        'API',
+        'Termos de serviço',
+        'Política de Privacidade',
+        'Política de cookies',
+      ],
+    },
+    {
+      title: 'Diretórios',
+      links: [
+        'Empregos de desenvolvimento',
+        'Desenvolvedores de aluguel',
+        'Desenvolvedores freelancers para contratar',
+        'Tag',
+        'Lugares',
+      ],
+    },
+    {
+      title: 'Ativos de Desenvolvimento',
+      links: [
+        'Mercado de códigos',
+        'Mercado do GitHub',
+        'Registro NPM',
+        'Pacofobia',
+      ],
+    },
+    {
+      title: 'Recursos de Desenvolvimento',
+      links: [
+        'Autônomo',
+        'Contratação de Desenvolvimento',
+        'Carteira de Desenvolvimento',
+        'Educação para o Desenvolvimento',
+        'Processo criativo',
+        'Tendências da Indústria de Desenvolvimento',
+      ],
+    },
+  ];
+  
